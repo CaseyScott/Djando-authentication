ed68be287f54500817c399185049630d3d6e50da
CSsnakeeater <34281480+CSsnakeeater@users.noreply.github.com>
2019-09-25 20:05:05 +1200

    Template Inheritance set up base.html using extend base.html

--- accounts/views.py
+++ accounts/views.py
@@ -17,13 +17,13 @@ def login(request):
     """Return a login page"""
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
-        
+
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password'])
-            
+
             messages.success(request, "You have successfully logged in!")
-            
+
             if user:
                 auth.login(user=user, request=request)
             else:
